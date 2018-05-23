5a9a2114ed0335f7ecfca5ce6f4c647b18c6540a
Martin Villalba <14tinchov@gmail.com>
2018-05-22 23:33:29 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import './App.css';
+import Header from './components/header/Header';
+import ProductView from './components/product_view/ProductView';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <ProductView/>
+      </div>
+    );
+  }
+}
+
+export default App;
